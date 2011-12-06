47e9fea16a2e68e26a17594b8141b7212089b6d8
Kevin Welsh <kev@microexpress.(none)>
2011-12-06 11:19:59 -0500

    simplified print statements

--- test.c
+++ test.c
@@ -18,23 +18,24 @@ display_info(const char *fpath, const struct stat *sb,
 
 //Type INode Name Size Blocks Block-Size Depth Offset Mode UID GID Path
 
-    printf("%s\t %-8ld\t %-20s\t %-10lld\t %-5lld\t %-5ld\t %-6d\t %-5d\t %-5ld\t %-5ld\t %-5ld\t %s\n",
+    printf("%s\t",
         (tflag == FTW_D) ?   "dir"   : (tflag == FTW_DNR) ? "dir(cannot read)" :
         (tflag == FTW_DP) ?  "dir(specified depth)"  : (tflag == FTW_F) ?   "file" :
         (tflag == FTW_NS) ?  "not symbolic"  : (tflag == FTW_SL) ?  "symbolic" :
-        (tflag == FTW_SLN) ? "symbolic(no file)" : "???",
-	(long) sb->st_ino,	
-	fpath + ftwbuf->base,
-	(long long) sb->st_size,
-	(long long) sb->st_blocks,
-	(long) sb->st_blksize,       
-	ftwbuf->level,  
-	ftwbuf->base,
-	(unsigned long) sb->st_mode,
-	(long) sb->st_uid,
-	(long) sb->st_gid,
-	fpath
-	);
+        (tflag == FTW_SLN) ? "symbolic(no file)" : "???");	//Type
+	
+	printf("%-8ld\t",(long) sb->st_ino);			//INode
+	printf("%-20s\t", fpath + ftwbuf->base);		//Name
+	printf("%-10lld\t", (long long) sb->st_size);		//Size
+	printf("%-5lld\t",(long long) sb->st_blocks);		//Blocks
+	printf("%-5ld\t",(long) sb->st_blksize);		//Block-Size
+	printf("%-6d\t",ftwbuf->level);				//Depth
+	printf("%-5d\t",ftwbuf->base);				//Offset
+	printf("%-5ld\t",(unsigned long) sb->st_mode);		//Mode
+	printf("%-5ld\t",(long)sb->st_uid);			//UID
+	printf("%-5ld\t",(long)sb->st_gid);			//GID
+	printf("%s \n",fpath);					//Path
+
     return 0;           /* To tell nftw() to continue */
 }
 
@@ -43,7 +44,20 @@ main(int argc, char *argv[])
 {
     int flags = 0;
 	printf("\n----------------------------------------------------------------\n");
-	printf("%s\t %-8s\t %-20s\t %-10s\t %-5s\t %-5s\t %-6s\t %-5s\t %-5s\t %-5s\t %-5s\t %s\n", "Type", "Inode", "Name", "Size(bytes)", "Blocks", "B-Size", "Depth", "Offset", "Mode", "UID", "GID", "Path");
+
+	printf("%s\t","Type");
+	printf("%-8s\t","INode");		
+	printf("%-20s\t","Name");		
+	printf("%-10s\t","Size(bytes)");	
+	printf("%-5s\t","Blocks");		
+	printf("%-5s\t","B-Size");		
+	printf("%-6s\t","Depth");		
+	printf("%-5s\t","Offset");		
+	printf("%-5s\t","Mode");		
+	printf("%-5s\t","UID");			
+	printf("%-5s\t","GID");			
+	printf("%s \n","Path");			
+
 
    if (argc > 2 && strchr(argv[2], 'd') != NULL)
         flags |= FTW_DEPTH;
